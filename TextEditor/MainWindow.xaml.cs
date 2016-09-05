46576d7d18a07d8a2bce2fbbf5029c35a9ec6473
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2016-09-04 17:11:45 -0700

    Add open file functionality

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace TextEditor
 {
@@ -20,9 +22,48 @@ namespace TextEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            //MessageBox.Show("The open command has been invoked on target object.");
+            Stream openFileStream = Stream.Null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            bool? userClickedOK = openFileDialog.ShowDialog();
+
+            if (userClickedOK == true)
+            {
+                try
+                {
+                    if ((openFileStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (openFileStream)
+                        {
+                            EditableTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void OpenCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
